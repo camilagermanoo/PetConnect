9342b0c46821ac140c15d7fe5d862a05eb9c14be
Mariana L.B <marianalibaniobarbosa@gmail.com>
2025-08-28 14:10:50 -0300

    Revert "Merge pull request #10 alteração de nome da branch para develop"
    This reverts commit 853f10899667f29d2dc4bd3240be60495284a59a, reversing
    changes made to 8e3f39be4abc75e1dced07946a3a7aa7e6260931.
    
    :wq#:wq

--- server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./src/app');
-
-// Define a porta a partir do ambiente ou usa 3000 como padrão
-const PORT = process.env.PORT || 3000;
-
-// Inicia o servidor e o faz ouvir na porta definida
-app.listen(PORT, () => {
-  console.log(`Servidor rodando com sucesso na porta ${PORT}`);
-});
