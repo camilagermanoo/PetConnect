e9a1cb973873b20b6e1a563fa8340e7982cbc4ce
joselucas-coder <sousajoselucas10@gmail.com>
2025-11-08 12:27:01 -0300

    Commit comentarios routes

--- src/models/AnimalPerdidoEncontrado.js
+++ src/models/AnimalPerdidoEncontrado.js
@@ -1,44 +1,44 @@
 const mongoose = require('mongoose');
 
 const animalPerdidoSchema = new mongoose.Schema({
-    nome:{
-        type:String,
-        required:true,
+    nome: {
+        type: String,
+        required: [true, 'O nome do animal é obrigatório.'],
         trim: true,
     },
-    especie:{
-        type:String,
-        required:true,
+    especie: {
+        type: String,
+        required: [true, 'A espécie do animal é obrigatória.'],
         trim: true,
     },
-    idade:{
-        type:Number,
-        required:true,
+    idade: {
+        type: Number,
+        required: [true, 'A idade do animal é obrigatória.'],
     },
-    porte:{
-        type:String,
-        enum:["Miniatura","Pequeno","Médio","Grande","Gigante"],
-        required:true,
+    porte: {
+        type: String,
+        enum: ["Pequeno", "Médio", "Grande"],
+        required: [true, 'O porte do animal é obrigatório.'],
     },
-    status:{
-        type:String,
-        required:true,
-        enum:["Encontrado", "Perdido"]
+    status: {
+        type: String,
+        required: [true, 'O status (Perdido/Encontrado) é obrigatório.'],
+        enum: ["Encontrado", "Perdido"]
     },
-    descricao:{
-        type:String,
-        required:true,
+    descricao: {
+        type: String,
+        required: [true, 'A descrição é obrigatória.'],
         trim: true,
     },
-    contato:{
-        type:String,
-        required:true,
-        trim:true
+    contato: {
+        type: String,
+        required: [true, 'A informação de contato é obrigatória.'],
+        trim: true
     },
-    local:{
-        type:String,
-        required:true,
-        trim:true
+    local: {
+        type: String,
+        required: [true, 'O local (onde foi perdido/encontrado) é obrigatório.'],
+        trim: true
     }
 }, { timestamps: true }); // Adiciona createdAt e updatedAt
 
